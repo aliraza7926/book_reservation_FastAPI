b877baa7f6e88dcbfcdefc1df1d9debe0d7ee0ae
aliraza7926 <aliraza7926@gmail.com>
2025-01-30 15:04:47 +0330

    update main.py with router to signup

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from api.auth import router as auth_router
 
-app = FastAPI()
-
+app = FastAPI(title="Book Reservation API", version="1.0.0")
+API_PREFIX = "/api/v1"
+app.include_router(auth_router, prefix=API_PREFIX + "/auth", tags=["Auth"])
 
 @app.get("/")
 async def root():
